53512cfed6eb0f579ada9db5a2c82110ebd81fd1
Marcela Melara <marcela.melara@intel.com>
2023-08-16 12:06:38 -0700

    Add support for remote subcmd for RD Go CLI
    Signed-off-by: Marcela Melara <marcela.melara@intel.com>

--- go/README.md
+++ go/README.md
@@ -11,10 +11,22 @@ go install
 
 ## Usage
 
-To generate an in-toto [Resource Descriptor] for a local file:
+scai-gen can be used to generate JSON encoded in-toto [Resource Descriptor]s,
+SCAI [Attribute Assertions], and SCAI [Attribute Reports].
+
+
+### Generate an in-toto Resource Descriptor
+
+Local file:
 
 ```bash
-scai-gen rd file -o <out-file> <filename> 
+scai-gen rd file -o <out-file> [-n <name>] [-u <URI>] [-l <download location>] [-t <media type>] <filename> 
+```
+
+Remote resource or service:
+
+```bash
+scai-gen rd remote -o <out-file> [-a <hash algorithm> -d <digest>] [-n <name>] <resource URI> 
 ```
 
 Run `scai-gen help` for a full list of command-line options.
--- go/cmd/rd.go
+++ go/cmd/rd.go
@@ -20,18 +20,51 @@ var rdCmd = &cobra.Command{
 var rdFileCmd = &cobra.Command{
 	Use:   "file",
 	Args:  cobra.ExactArgs(1),
-	Short: "Generate a JSON-encoded in-toto Resource Descriptor from local file(s)",
+	Short: "Generate a JSON-encoded in-toto Resource Descriptor for a file",
 	RunE:  genRdFromFile,
 }
 
+var rdRemoteCmd = &cobra.Command{
+	Use:   "remote",
+	Args:  cobra.ExactArgs(1),
+	Short: "Generate a JSON-encoded in-toto Resource Descriptor for a remote resource, identified via a URI, incl. git repos, container images, packages, and services",
+	RunE:  genRdForRemote,
+}
+
 var (
-	withContent          bool
+	digest           string
 	downloadLocation string
+	hashAlg          string
 	mediaType        string
+	name             string
 	outFile          string
+	uri              string
+	withContent      bool
 )
 
 func init() {
+	rdCmd.PersistentFlags().StringVarP(
+		&outFile,
+		"out-file",
+		"o",
+		"",
+		"Filename to write out the RD object",
+	)
+	rdCmd.MarkPersistentFlagRequired("out-file")
+
+	rdCmd.AddCommand(rdFileCmd)
+	rdCmd.AddCommand(rdRemoteCmd)
+}
+
+func init() {
+	rdFileCmd.Flags().StringVarP(
+		&uri,
+		"uri",
+		"u",
+		"",
+		"The URI of the resource",
+	)
+	
 	rdFileCmd.Flags().BoolVarP(
 		&withContent,
 		"content",
@@ -55,17 +88,41 @@ func init() {
 		"",
 		"The media type of the resource",
 	)
+}
 
-	rdCmd.PersistentFlags().StringVarP(
-		&outFile,
-		"out-file",
-		"o",
+func init() {
+	rdRemoteCmd.Flags().StringVarP(
+		&name,
+		"name",
+		"n",
 		"",
-		"Filename to write out the RD object",
+		"A name for the remote resource or service",
 	)
-	rdCmd.MarkPersistentFlagRequired("out-file")
 
-	rdCmd.AddCommand(rdFileCmd)
+	rdRemoteCmd.Flags().StringVarP(
+		&downloadLocation,
+		"download-location",
+		"l",
+		"",
+		"The download location of the remote resource (if different from the URI)",
+	)
+
+	rdRemoteCmd.Flags().StringVarP(
+		&digest,
+		"digest",
+		"d",
+		"",
+		"The digest associated with the remote resource (hex-encoded)",
+	)
+
+	rdRemoteCmd.Flags().StringVarP(
+		&hashAlg,
+		"hash-alg",
+		"a",
+		"",
+		"The hash algorithm used to compute the digest associated with the remote resource",
+	)
+	rdRemoteCmd.MarkFlagsRequiredTogether("digest", "hash-alg")
 }
 
 func genSHA256(bytes []byte) []byte {
@@ -91,6 +148,7 @@ func genRdFromFile(cmd *cobra.Command, args []string) error {
 	
 	rd := &ita.ResourceDescriptor{
 		Name: filename,
+		Uri: uri,
 		Digest: map[string]string{"sha256": strings.ToLower(sha256Digest)},
 		Content: content,
 		DownloadLocation: downloadLocation,
@@ -98,6 +156,38 @@ func genRdFromFile(cmd *cobra.Command, args []string) error {
 	}
 
 	err = rd.Validate()
+	if err != nil {
+		return fmt.Errorf("Invalid resource descriptor", err)
+	}
+	
+	return util.WritePbToFile(rd, outFile)
+}
+
+func genRdForRemote(cmd *cobra.Command, args []string) error {
+
+	remoteUri := args[0]
+
+	var digestSet map[string]string
+	if len(digest) > 0 {
+		// the in-toto spec expects a hex-encoded string in DigestSets
+		// https://github.com/in-toto/attestation/blob/main/spec/v1/digest_set.md
+		_, err := hex.DecodeString(digest)
+		if err != nil {
+			return fmt.Errorf("Hash is not valid hex-encoded string", err)
+		}
+		
+		// we can assume that we have both variables set at this point
+		digestSet = map[string]string{hashAlg: strings.ToLower(digest)}
+	}
+	
+	rd := &ita.ResourceDescriptor{
+		Name: name,
+		Uri: remoteUri,
+		Digest: digestSet,
+		DownloadLocation: downloadLocation,
+	}
+
+	err := rd.Validate()
 	if err != nil {
 		return fmt.Errorf("Invalid resource descriptor", err)
 	}
